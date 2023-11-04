6c4c382e50c266d35556cfcb75379dd38569fb4c
Sam Spaethe <sspaethe@gmail.com>
2023-11-03 20:34:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,18 +5,21 @@ const chart = new Chart(ctx, {
         datasets: [{
             label: 'Guess the Date & Weight',
             data: [
-                    { x: new Date(2023, 10, 22), y: 8 },
-                    { x: new Date(2023, 10, 29), y: 7.5 },
-                    { x: new Date(2023, 11, 6), y: 7.81 },
-                    { x: new Date(2023, 10, 27), y: 7.88 },
-                    { x: new Date(2023, 11, 26), y: 8.75 },
-                    { x: new Date(2023, 10, 19), y: 8.31 },
-                    { x: new Date(2023, 10, 23), y: 6.75 },
-                    { x: new Date(2023, 11, 4), y: 8.13 },
-                    { x: new Date(2023, 11, 6), y: 8.31 },
-                    { x: new Date(2023, 10, 30), y: 7.69 },
-                    { x: new Date(2023, 11, 3), y: 6.56 },
-                    { x: new Date(2023, 11, 8), y: 6.56 }
+                    { x: new Date(2023, 11, 22), y: 8 , person: "Capt Marquart"},
+                    { x: new Date(2023, 11, 29), y: 7.5 , person: "Marshall"},
+                    { x: new Date(2023, 12, 6), y: 7.81 , person: "Palmer"},
+                    { x: new Date(2023, 11, 27), y: 7.88 , person: "Kari"},
+                    { x: new Date(2023, 12, 20), y: 8.75 , person: "Michele"},
+                    { x: new Date(2023, 11, 19), y: 8.31 , person: "Erica"},
+                    { x: new Date(2023, 11, 28), y: 6.75 , person: "Leah"},
+                    { x: new Date(2023, 12, 4), y: 8.13 , person: "Arambula"},
+                    { x: new Date(2023, 12, 10), y: 8 , person: "Kim"},
+                    { x: new Date(2023, 12, 6), y: 8.31 , person: "Lynn"},
+                    { x: new Date(2023, 12, 8), y: 7.31 , person: "Calicia"},
+                    { x: new Date(2023, 11, 30), y: 7.69 , person: "Raven"},
+                    { x: new Date(2023, 12, 3), y: 6.56 , person: "Varnessa"},
+                    { x: new Date(2023, 12, 8), y: 6.56 , person: "McLean"},
+                    { x: new Date(2023, 12, 10), y: 7.25 , person: "Shanette"}
             ],
             backgroundColor: 'rgba(75, 192, 192, 0.5)',
             borderColor: 'rgb(75, 192, 192)',
@@ -45,7 +48,21 @@ const chart = new Chart(ctx, {
             title: {
                 display: true,
                 text: 'Bragging Rights: Guess the Date & Weight'
+            },
+            tooltip: {
+            callbacks: {
+                title: function(tooltipItem, data) {
+                    // Extract the date from the data and format it without the time.
+                    let date = data.datasets[tooltipItem[0].datasetIndex].data[tooltipItem[0].index].x;
+                    return moment(date).format('MMM D, YYYY');  // Display date without time
+                },
+                afterBody: function(tooltipItem, data) {
+                    // Extract the person's name from the data and display it below the main tooltip text.
+                    let person = data.datasets[tooltipItem[0].datasetIndex].data[tooltipItem[0].index].person;
+                    return 'Guessed by: ' + person;
+                }
             }
+        }
         }
     }
 });
